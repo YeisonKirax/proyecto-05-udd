a43f3cef7bdd46c246d2e9789f6efe401f156128
Yeison Fernandez <yeisonfj@icloud.com>
2023-06-14 20:10:58 -0400

    agregado componentes vacios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header/Header.jsx'
+import { Login } from './pages/Auth/Login/Login.jsx'
+import { SignUp } from './pages/Auth/SignUp/SignUp.jsx'
+import { Home } from './pages/Home/Home.jsx'
+import Product from './pages/Products/Product/Product.jsx'
+import { Profile } from './pages/Profile/Profile.jsx'
 function App() {
 
   return (
-    <>
+    <div className='App'>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          {/* Rutas públicas */ }
+          <Route path='/home' element={ <Home></Home> }>
+          </Route>
+          <Route path='/products' element={ <Products></Products> }>
+          </Route>
+          <Route path='/products/:id' element={ <Product></Product> }>
+          </Route>
+          <Route path='/auth/login' element={ <Login></Login> }>
+          </Route>
+          <Route path='/auth/signup' element={ <SignUp></SignUp> }>
+          </Route>
 
-    </>
+          {/* Rutas privada */ }
+          <Route path='/profile' element={ <Profile></Profile> }>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/Products/Product/Product.jsx
@@ -0,0 +1,5 @@
+export const Product = () => {
+  return <h1>Productos</h1>
+}
+
+export default Product
