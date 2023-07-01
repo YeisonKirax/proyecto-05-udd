8f43decc6172cee9760c38aa6bf02a94748d9298
Yeison Fernandez <yeisonfj@icloud.com>
2023-07-01 09:33:32 -0400

    agregado cliente axios

---
+++ src/config/axios.jsx
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const backendUrlLocal = "http://locahost:4500"
+const backendUrlProd = "https://api-express-6zho.onrender.com"
+const axiosClient = axios.create( {
+  baseURL: backendUrlLocal
+} )
+
+export default axiosClient
